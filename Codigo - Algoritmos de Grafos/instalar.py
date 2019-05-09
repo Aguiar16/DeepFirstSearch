54d4d5018def3aa4eee42183ec2d58ee956bdd2d
Gabriel Lins <38196422+RickSaitamaSanchez@users.noreply.github.com>
2019-05-09 14:21:09 -0300

    Update instalar.py

--- Codigo - Algoritmos de Grafos/instalar.py
+++ Codigo - Algoritmos de Grafos/instalar.py
@@ -1,5 +1,5 @@
 import os
 
-instalacoes = "sudo apt-get install -y python3-tk libfreetype6-dev pkg-config libpng12-dev vlc && pip3 install --user -r requirements.txt"
+instalacoes = "sudo apt-get install -y python3-pip python3-tk libfreetype6-dev pkg-config libpng12-dev vlc && pip3 install --user -r requirements.txt"
 
 os.system(instalacoes)
